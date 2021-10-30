5f8eb1e673ae616b9f998d11bfeb88f2be978af7
Maximilian Wright <twixthehero@gmail.com>
2021-10-30 02:23:33 -0600

    Add Java library dependency example

--- README.md
+++ README.md
@@ -8,6 +8,31 @@ Example Bazel workspace configured to build a Kotlin program binary.
 
 ```bash
 $ bazel run //example
+$ bazel run //example -- -h
+```
+
+### Adding new Java dependencies
+
+1. Add the new dependency as a `maven_install` artifact in `WORKSPACE`.
+
+```starlark
+maven_install(
+    artifacts = [
+        "commons-cli:commons-cli:1.5.0",
+        "<groupId>:<artifactId>:<version>",
+    ],
+)
+```
+
+2. Use the new dependency. Convert all dashes (`-`) to underscores (`_`).
+
+```starlack
+java_library(
+   deps = [
+      "@maven//:commons_cli_commons_cli",
+      "@maven//:<group_id>_<artifact_id>",
+   ],
+)
 ```
 
 # License
--- example/main.kt
+++ example/main.kt
@@ -1,5 +1,31 @@
 package com.krythera.example
 
-fun main() {
-    println("hello krythera")
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.HelpFormatter
+import org.apache.commons.cli.Options
+import kotlin.system.exitProcess
+
+private const val optHelpShort = "h"
+private const val optHelpLong = "help"
+private const val optRespectShort = "f"
+private const val optRespectLong = "respect"
+
+fun main(args: Array<String>) {
+    val opts = Options().apply {
+        addOption(optHelpShort, optHelpLong, false, "Display this help.")
+        addOption(optRespectShort, optRespectLong, false, "To pay respects.")
+    }
+
+    val cmd = DefaultParser().parse(opts, args)
+
+    if (cmd.hasOption(optHelpShort)) {
+        HelpFormatter().printHelp("bazel run //example --", "\nRun the Bazel Kotlin example", opts, "", true)
+        exitProcess(1)
+    }
+
+    if (cmd.hasOption(optRespectShort)) {
+        println("Respects paid.")
+    } else {
+        println("Respects not paid.")
+    }
 }
